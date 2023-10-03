e038f8c481d884876c046ab08584ecdfb22e2694
aarontaskerlynch <aarontaskerlynch@hotmail.com>
2023-10-03 21:55:11 +0100

    Folder structure updates and camera update to enhance playability

--- Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset = new Vector3(35,10, 10);
+    private Vector3 offset = new Vector3(100,10, 40);
 
     // Start is called before the first frame update
     void Start()
